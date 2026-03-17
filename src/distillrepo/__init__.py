a29ba23cb86baaf03c8f9088ac2e3f69f828a572
Pradeep Reddy Raamana <raamana@gmail.com>
2026-03-17 17:41:26 -0400

    new release

--- src/distillrepo/__init__.py
+++ src/distillrepo/__init__.py
@@ -8,6 +8,6 @@ try:
     __version__ = version("distillrepo")
 except PackageNotFoundError:
     # Running from source (not installed as a distribution).
-    __version__ = "0.1.2"
+    __version__ = "0.2"
 
 __all__ = ["__version__", "analyze", "bundle"]
