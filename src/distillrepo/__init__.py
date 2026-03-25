707bd71d0ab713861529e25a6c539831efe3a92f
Pradeep Reddy Raamana <raamana@gmail.com>
2026-03-25 08:55:17 -0400

    Bump version to 0.25

--- src/distillrepo/__init__.py
+++ src/distillrepo/__init__.py
@@ -8,6 +8,6 @@ try:
     __version__ = version("distillrepo")
 except PackageNotFoundError:
     # Running from source (not installed as a distribution).
-    __version__ = "0.2"
+    __version__ = "0.25"
 
 __all__ = ["__version__", "analyze", "bundle"]
